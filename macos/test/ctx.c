ea60c4179dad1f07f1ceb28685a725d785bfeef2
karlthilking <thilking.k@northeastern.edu>
2026-03-24 02:06:19 -0400

    testing /proc/self/maps equivalent for macos

--- macos/test/ctx.c
+++ macos/test/ctx.c
@@ -2,48 +2,50 @@
 #include <ucontext.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
+#include <ptrauth.h>
 
-char *regs[28] =
+ucontext_t uc;
+
+char *regs[33] =
 {
-        "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7",
-        "x8", "x9", "x15", "x16", "x17", "x18", "x19",
-        "x20", "x21", "x22", "x23", "x24", "x25", "x26",
-        "x27", "x28", "fp", "lr", "sp", "pc"
+        "x0", "x1", "x2", "x3", "x4", "x5", "x6","x7","x8","x9",
+        "x10","x11", "x12", "x13", "x14", "x15", "x16", "x17",
+        "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25",
+        "x26", "x27", "x28", "fp", "lr", "sp", "pc"
 };
 
-/**
- * 
- *
- *
- *
- *
- *
- *
- */
+void print_ucontext(ucontext_t *uc)
+{
+        mcontext_t mc = uc->uc_mcontext;
+
+        for (int i = 0; i < 29; i++)
+                printf("%s:\t%llu\n", regs[i], mc->__ss.__x[i]);
+        
+        printf("%s:\t%llu\n", regs[29], mc->__ss.__fp);
+        printf("%s:\t%llu\n", regs[30], mc->__ss.__lr);
+        printf("%s:\t%llu\n", regs[31], mc->__ss.__sp);
+        printf("%s:\t%llu\n", regs[32], mc->__ss.__pc);
+
+        // ptrauth_strip(IB, mc->__ss.__lr);
+}
+
+void recursive(int levels)
+{
+        if (levels > 0)
+                recursive(levels - 1);
+        else {
+                getcontext(&uc);
+                printf("Context after 1000 recursive calls\n");
+                print_ucontext(&uc);
+        }
+        return;
+}
+
 int main(void)
 {
-        srand(time(NULL));
-        
-        int A[1024];
-        ucontext_t uc;
-        
-        for (int i = 1; i < 1024; i++) {
-                *(A + i) = rand() % 1024;
-                *(A + i - 1) = *(A + i) * 2 + 3;
-        }
-        
-        static int count;
-        count = 0;
+        printf("Starting context\n");
         getcontext(&uc);
-        printf("%d\n", count++);
-        
-        // for (int i = 0; i < 31; i++)
-        //         printf("%llu\n", uc.uc_mcontext->gregs[i]);
-        // printf("%llu\n", uc.uc_mcontext->sp);
-        // printf("%llu\n", uc.uc_mcontext->pc);
-
-        if (count == 1)
-                setcontext(&uc);
+        print_ucontext(&uc);
+        recursive(1000);
         exit(0);
 }
---
+++ macos/test/maps.c
@@ -0,0 +1,145 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <mach/mach.h>
+#include <mach/mach_vm.h>
+#include <mach/vm_region.h>
+#include <mach/vm_statistics.h>
+#include <mach/shared_region.h>
+#include <mach-o/dyld.h>
+#include <libproc.h>
+
+// #ifndef VM_MEMORY_DYLD
+// #define VM_MEMORY_DYLD          60
+// #endif
+
+// #ifndef VM_MEMORY_DYLD_MALLOC
+// #define VM_MEMORY_DYLD_MALLOC   61
+// #endif
+
+typedef struct {
+        mach_vm_address_t       start;
+        mach_vm_address_t       end;
+        mach_vm_size_t          size;
+        vm_prot_t               prot;
+        unsigned int            tag;
+        char                    name[128];
+} mem_rgn_t;
+
+void mem_rgn_name(mem_rgn_t *r)
+{
+        int rc = proc_regionfilename(getpid(),
+                                     r->start,
+                                     r->name,
+                                     sizeof(r->name));
+        
+        if (rc > 0)
+                return;
+
+        switch (r->tag) {
+        case VM_MEMORY_MALLOC:
+        case VM_MEMORY_MALLOC_NANO:
+        case VM_MEMORY_MALLOC_TINY:
+        case VM_MEMORY_MALLOC_SMALL:
+        case VM_MEMORY_MALLOC_LARGE:
+        case VM_MEMORY_MALLOC_LARGE_REUSABLE:
+        case VM_MEMORY_MALLOC_LARGE_REUSED:
+                snprintf(r->name, sizeof(r->name), "[heap]");
+                return;
+        case VM_MEMORY_STACK:
+                snprintf(r->name, sizeof(r->name), "[stack]");
+                return;
+        case VM_MEMORY_GUARD:
+                snprintf(r->name, sizeof(r->name), "[guard]");
+                return;
+        // case VM_MEMORY_DLYD:
+        //         snprintf(r->name, sizeof(r->name), "[dyld]");
+        //         return;
+        // case VM_MEMORY_DLYD_MALLOC:
+        //         snprintf(r->name, sizeof(r->name), "[dyld_heap]");
+        //         return;
+        case VM_MEMORY_SHARED_PMAP:
+                snprintf(r->name, sizeof(r->name), "[shared_pmap]");
+                return;
+        default:
+                break;
+        }
+
+        if (r->start == 0 && r->prot == VM_PROT_NONE) {
+                snprintf(r->name, sizeof(r->name), "[zeropage]");
+                return;
+        }
+
+        snprintf(r->name, sizeof(r->name), "[anonymous]");
+        return;
+}
+
+void print_one_region(mem_rgn_t *r)
+{
+        char rwx[3];
+        rwx[0] = (r->prot & VM_PROT_READ) ? 'r' : '-';
+        rwx[1] = (r->prot & VM_PROT_WRITE) ? 'w' : '-';
+        rwx[2] = (r->prot & VM_PROT_EXECUTE) ? 'x' : '-';
+        
+        mem_rgn_name(r);
+
+        printf("%llx-%llx %c%c%c %s\n",
+                r->start, r->end,
+                rwx[0], rwx[1], rwx[2],
+                r->name);
+}
+
+void print_memory_regions()
+{
+        mach_vm_address_t               addr    = 0;
+        mach_vm_size_t                  size    = 0;
+        natural_t                       depth   = 0;
+        vm_region_submap_info_data_64_t info;
+        mach_msg_type_number_t          count;
+        kern_return_t                   kr;
+
+        for (;;) {
+                count = VM_REGION_SUBMAP_INFO_COUNT_64;
+                kr = mach_vm_region_recurse(
+                        mach_task_self(),
+                        &addr,
+                        &size,
+                        &depth,
+                        (vm_region_recurse_info_t)&info,
+                        &count
+                );
+
+                if (kr == KERN_INVALID_ADDRESS)
+                        break;
+                else if (kr != KERN_SUCCESS) {
+                        fprintf(stderr,
+                                "mach_vm_region_recurse: %s\n",
+                                mach_error_string(kr));
+                        break;
+                }
+
+                if (info.is_submap) {
+                        depth += 1;
+                        continue;
+                }
+
+                mem_rgn_t r;
+
+                r.start = addr;
+                r.size  = size;
+                r.end   = addr + size;
+                r.prot  = info.protection;
+
+                print_one_region(&r);
+
+                addr += size;
+        }
+}
+
+int main(void)
+{
+        print_memory_regions();
+        exit(0);
+}
+
