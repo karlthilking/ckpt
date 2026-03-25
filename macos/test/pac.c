3bb3982aa57cae11cf7e6ab8f046575459522807
karlthilking <thilking.k@northeastern.edu>
2026-03-25 01:18:44 -0400

    added point authentication macros

--- macos/test/pac.c
+++ macos/test/pac.c
@@ -1,25 +1,89 @@
 #define _XOPEN_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
 #include <ucontext.h>
 #include <ptrauth.h>
+
+typedef uint64_t u64;
+
+/* Strip PAC from pointer to instruction address */
+#define PACSTRIP_I(ptr) \
+        do { \
+                __asm__ __volatile__ ( \
+                        "xpaci %0" \
+                        : "+r" (ptr) \
+                        : \
+                        : "memory" \
+                ); \
+        } while (0)
+
+/* Strip PAC from pointer to data address */
+#define PACSTRIP_D(ptr) \
+        do { \
+                __asm__ __volatile__ ( \
+                        "xpacd %0" \
+                        : "+r" (ptr) \
+                        : \
+                        : "memory" \
+                ); \
+        } while (0)
+
+/* Sign pointer with IA key using a modifier */
+#define PACSIGN_IA(ptr, modifier) \
+        do { \
+                __asm__ __volatile__ ( \
+                        "pacia %0, %1" \
+                        : "+r" (ptr) \
+                        : "r"  (modifier) \
+                        : "memory" \
+                ); \
+        } while (0)
+
+/* Sign pointer with IB key using a modifier */
+#define PACSIGN_IB(ptr, modifier) \
+        do { \
+                __asm__ __volatile__ ( \
+                        "pacib %0, %1" \
+                        : "+r" (ptr) \
+                        : "r"  (modifier) \
+                        : "memory" \
+                ); \
+        } while (0)
+
+#define PACSIGNED(ptr) ((u64)(ptr) > (((u64)(1) << 48) - 1))
 
 int main(void)
 {
         ucontext_t uc;
+        getcontext(&uc);
         mcontext_t mc = uc.uc_mcontext;
         
-        getcontext(&uc);
-        printf("lr:\t\t%p\n", mc->__ss.__opaque_lr);
+        printf("original lr:\t%llu\n", mc->__ss.__lr);
+        assert(PACSIGNED(mc->__ss.__lr));
+        
+        PACSTRIP_I(mc->__ss.__lr);
+        printf("stripped lr:\t%llu\n", mc->__ss.__lr);
+        assert(!PACSIGNED(mc->__ss.__lr));
+        
+        PACSIGN_IB(mc->__ss.__lr, mc->__ss.__sp);
+        printf("re-signed lr:\t%llu\n", mc->__ss.__lr);
+        assert(PACSIGNED(mc->__ss.__lr));
 
-        mc->__ss.__opaque_lr = ptrauth_strip(&mc->__ss.__opaque_lr, 
-                                             ptrauth_key_asib);
-        printf("stripped lr:\t%p\n", mc->__ss.__opaque_lr);
+        for (int i = 0; i < 29; i++) {
+                if (PACSIGNED(mc->__ss.__x[i])) {
+                        printf("x%d:\t%llu\n",
+                                i, mc->__ss.__x[i]);
+                }
+        }
 
-        mc->__ss.__opaque_lr = ptrauth_sign_unauthenticated(&mc->__ss.__opaque_lr,
-                                                            ptrauth_key_asib,
-                                                            mc->__ss.__opaque_sp);
-        printf("signed lr:\t%p\n", mc->__ss.__opaque_lr);
-
+        if (PACSIGNED(mc->__ss.__sp))
+                printf("signed sp:\t%llu\n", mc->__ss.__sp);
+        if (PACSIGNED(mc->__ss.__fp))
+                printf("signed fp:\t%llu\n", mc->__ss.__fp);
+        if (PACSIGNED(mc->__ss.__pc))
+                printf("signed pc:\t%llu\n", mc->__ss.__pc);
+        
         exit(0);
 }
