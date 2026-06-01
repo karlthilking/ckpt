aa1bcdb58020c930e632d18ac0ebef61e591b9cb
karlthilking <thilking.k@northeastern.edu>
2026-06-01 17:25:19 -0400

    change time wrappers to more apparent names

--- src/time_wrappers.c
+++ src/time_wrappers.c
@@ -7,59 +7,61 @@
 #include <unistd.h>
 #include "time_wrappers.h"
 
-unsigned int __sleep(unsigned int seconds)
+unsigned int __sleep_hook(unsigned int seconds)
 {
-        int             retval;
+        int             err;
         struct timespec rq = { seconds, 0 };
         struct timespec rm = { 0, 0 };
-        
-        retval = __nanosleep(&rq, &rm);
-        if (retval != 0)
+
+        err = __nanosleep_hook(&rq, &rm);
+        if (err != 0) {
                 return rm.tv_sec;
+        }
 
         return 0;
 }
 
-int __usleep(useconds_t microseconds)
+int __usleep_hook(useconds_t usec)
 {
-        int             retval;
+        int             err;
+        long            nsec;
         struct timespec rq, rm = { 0, 0 };
-        long            nanos;
 
-        nanos           = microseconds * NSEC_PER_USEC;
-        rq.tv_sec       = nanos / NSEC_PER_SEC;
-        rq.tv_nsec      = nanos % NSEC_PER_SEC;
+        nsec = usec * NSEC_PER_USEC;
+        rq.tv_sec = nsec / NSEC_PER_SEC;
+        rq.tv_nsec = nsec % NSEC_PER_SEC;
 
-        retval = __nanosleep(&rq, &rm);
-        if (retval != 0)
+        err = __nanosleep_hook(&rq, &rm);
+        if (err != 0) {
                 return -1;
+        }
 
         return 0;
 }
 
-int __nanosleep(const struct timespec *rqtp, struct timespec *rmtp)
+int __nanosleep_hook(const struct timespec *rqtp, struct timespec *rmtp)
 {
-        int                     retval;
-        struct timeval          tv;
-        struct timespec         start, end;
+        int             err;
+        struct timeval  tv;
+        struct timespec start, end;
 
         TIMESPEC_TO_TIMEVAL(rqtp, &tv);
-        if (rmtp != NULL)
+        if (rmtp != NULL) {
                 clock_gettime(CLOCK_MONOTONIC, &start);
-        
-        retval = select(0, NULL, NULL, NULL, &tv);
-        if (retval != 0 && errno == EINTR && rmtp) {
-                long rq, el, rm;
+        }
+
+        err = select(0, NULL, NULL, NULL, &tv);
+        if (err != 0 && errno == EINTR && rmtp != NULL) {
+                long rq, rm, elapsed;
 
                 clock_gettime(CLOCK_MONOTONIC, &end);
-                
                 rq = TIMESPEC_TO_NSEC(rqtp);
-                el = TIMESPEC_NSEC_DIFF(&end, &start);
-                rm = rq - el;
-                
+                elapsed = TIMESPEC_NSEC_DIFF(&end, &start);
+                rm = rq - elapsed;
+
                 rmtp->tv_sec = rm / NSEC_PER_SEC;
                 rmtp->tv_nsec = rm % NSEC_PER_SEC;
         }
 
-        return retval;
+        return err;
 }
