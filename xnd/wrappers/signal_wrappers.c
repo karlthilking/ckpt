5a022f1150abdd4bee071c8f04b3ed7157323bb2
karlthilking <thilking.k@northeastern.edu>
2026-07-24 01:27:50 -0400

    Change signal wrappers to use variable checkpoint signal

--- xnd/wrappers/signal_wrappers.c
+++ xnd/wrappers/signal_wrappers.c
@@ -20,65 +20,55 @@ static struct sigaction sa_table[NSIG];
 
 void sig_state_save(void)
 {
-        int err;
+	int sig;
 
-        for (int sig = 1; sig < NSIG; sig++) {
-                if (sig == SIGKILL || sig == SIGSTOP) {
-                        continue;
-                } 
-                err = __xnd_sigaction(sig, NULL, &sa_table[sig]);
-                if (err != 0) {
-                        xnd_warn("Failed to save signal disposition "
-                                 "for signal %d\n", sig);
-                        bzero(&sa_table[sig], sizeof(struct sigaction));
-                }
-        }
+	for (sig = 1; sig < NSIG; sig++) {
+		if (sig == SIGKILL || sig == SIGSTOP)
+			continue;
+		if (__xnd_sigaction(sig, NULL, &sa_table[sig]) != 0) {
+			xnd_warn("Error saving signal disposition "
+				 "(signal: %d)\n", sig);
+			bzero(&sa_table[sig], sizeof(sa_table[sig]));
+		}
+	}
 }
 
 void sig_state_restore(void)
 {
-        int err;
+	int sig;
 
-        for (int sig = 1; sig < NSIG; sig++) {
-                if (sig == SIGKILL || sig == SIGSTOP) {
-                        continue;
-                } 
-                err = __xnd_sigaction(sig, &sa_table[sig], NULL);
-                if (err != 0) {
-                        xnd_warn("Failed to restore signal disposition "
-                                 "for signal %d\n", sig);
-                }
-        }
+	for (sig = 1; sig < NSIG; sig++) {
+		if (sig == SIGKILL || sig == SIGSTOP)
+			continue;
+		if (__xnd_sigaction(sig, &sa_table[sig], NULL) != 0)
+			xnd_warn("Error restoring signal disposition "
+				 "(signal: %d)\n", sig);
+	}
 }
 
 sig_t __signal_hook(int sig, sig_t handler)
 {
-        struct sigaction        sa;
-        int                     err;
-        char                    *program;
+	struct sigaction sa;
 
-        if (sig == SIGUSR1 || sig == SIGUSR2) {
-                xnd_warn("signal %d is reserved (%s)\n",
-                         sig, (sig == SIGUSR2) ? "SIGUSR2" : "SIGUSR1");
-                return SIG_ERR;
-        } else if (handler == SIG_DFL || handler == SIG_IGN) {
-                return signal(sig, handler);
-        }
-        
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = 0;
-        sa.sa_handler = handler;
-        err = __xnd_sigaction(sig, &sa, NULL);
-        if (err != 0) {
-                return SIG_ERR;
-        }
-        
-        if ((program = env_get_program_name()) != NULL) {
-                xnd_trace("%s installed handler for signal %d: %p\n",
-                          program, sig, handler);
-        }
-        
-        return handler;
+	if (sig == env_get_ckpt_signal()) {
+		xnd_warn("Signal %d is reserved\n", sig);
+		return SIG_ERR;
+	}
+
+	if (handler == SIG_DFL || handler == SIG_IGN)
+		return signal(sig, handler);
+
+	/**
+	 * Register user signal handlers with __xnd_sigaction
+	 * so we can control the signal trampoline function
+	 */
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = handler;
+	if (__xnd_sigaction(sig, &sa, NULL) != 0)
+		return SIG_ERR;
+
+	return 0;
 }
 
 /**
@@ -89,34 +79,28 @@ sig_t __signal_hook(int sig, sig_t handler)
 int __sigaction_hook(int sig, const struct sigaction *act,
                      struct sigaction *oact)
 {
-        int     err;
-        char    *program;
-        
-        if (!act) {
-                return sigaction(sig, NULL, oact);
-        }
+	/**
+	 * This makes the thread signal handler (in thread_info.c)
+	 * not transparent to user threads; maybe should hide the
+	 * signal handler?
+	 */
+	if (act == NULL)
+		return sigaction(sig, NULL, oact);
 
-        if (sig == SIGUSR1 || sig == SIGUSR2) {
-                xnd_warn("signal %d is reserved (%s)\n",
-                         sig, (sig == SIGUSR2) ? "SIGUSR2" : "SIGUSR1");
-                return -1;
-        }
+	if (sig == env_get_ckpt_signal()) {
+		xnd_warn("Signal %d is reserved\n", sig);
+		return -1;
+	}
 
-        if (act->sa_handler == SIG_DFL || act->sa_handler == SIG_IGN) {
-                return sigaction(sig, act, oact);
-        }
-        
-        err = __xnd_sigaction(sig, act, oact);
-        if (err != 0) {
-                return err;
-        }
-        
-        if ((program = env_get_program_name()) != NULL) {
-                xnd_trace("%s installed handler for signal %d: %p\n",
-                          program, sig, act->sa_handler);
-        }
-        
-        return err;
+	if (act->sa_handler == SIG_DFL || act->sa_handler == SIG_IGN)
+		return sigaction(sig, act, oact);
+
+	/**
+	 * If user thread is registering their own signal handler,
+	 * call __xnd_sigaction instead of sigaction in order to
+	 * avoid libc's signal trampoline (_sigtramp)
+	 */
+	return __xnd_sigaction(sig, act, oact);
 }
 
 int __sigprocmask_hook(int how, const sigset_t *set, sigset_t *oset)
@@ -126,48 +110,19 @@ int __sigprocmask_hook(int how, const sigset_t *set, sigset_t *oset)
 
 int __pthread_sigmask_hook(int how, const sigset_t *set, sigset_t *oset)
 {
-        sigset_t clean;
+	sigset_t clean;
+	int ckpt_sig = env_get_ckpt_signal();
 
-        if (!set) {
-                return pthread_sigmask(how, NULL, oset);
-        }
+	if (set == NULL)
+		return pthread_sigmask(how, NULL, oset);
 
-        if (sigismember(set, SIGUSR1) || sigismember(set, SIGUSR2)) {
-                xnd_trace("User thread is attempting to manipulate "
-                          "a xnd reserved signal number\n");
-        }
-        
-        clean = *set;
-        switch (how) {
-        case SIG_BLOCK:
-                if (sigismember(set, SIGUSR1)) {
-                        xnd_trace("SIGUSR1 can not be blocked for the "
-                                  "implementation of xnd\n");
-                        sigdelset(&clean, SIGUSR1);
-                }
-                break;
-        case SIG_UNBLOCK:
-                if (sigismember(set, SIGUSR2)) {
-                        xnd_trace("SIGUSR2 must be kept blocked for the"
-                                  "implementation of xnd\n");
-                        sigdelset(&clean, SIGUSR2);
-                }
-                break;
-        case SIG_SETMASK:
-                if (sigismember(set, SIGUSR1)) {
-                        xnd_trace("SIGUSR1 can not be blocked for the "
-                                  "implementation of xnd\n");
-                        sigdelset(&clean, SIGUSR1);
-                }
-                if (!sigismember(set, SIGUSR2)) {
-                        xnd_trace("SIGUSR2 must be kept blocked for the "
-                                  "implementation of xnd\n");
-                        sigaddset(&clean, SIGUSR2);
-                }
-                break;
-        }
-        
-        return pthread_sigmask(how, &clean, oset);
+	clean = *set;
+	if (how == SIG_BLOCK || how == SIG_SETMASK) {
+		if (sigismember(set, ckpt_sig))
+			sigdelset(&clean, ckpt_sig);
+	}
+
+	return pthread_sigmask(how, &clean, oset);
 }
 
 INTERPOSE(__signal_hook, signal);
